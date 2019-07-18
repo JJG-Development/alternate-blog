005a45ee97829973c2c13d7db77dca286ab1464a
JJG Development <jjg.development@gmail.com>
2019-07-18 13:55:40 -0700

    Add importance and keyword validations.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
     validates :title, presence: true, length: {minimum: 3, maximum: 50}
     validates :description, presence: true, length: {minimum: 10, maximum: 300}
     validates :keyword, presence: true
-
+    validates :importance, numericality: {less_than_or_equal_to: 5}
 end
