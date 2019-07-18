a5f3385df2d240ef6f40ffb1d62c8248a2610dd8
JJG Development <jjg.development@gmail.com>
2019-07-18 16:42:34 -0700

    Custom error message in validates :keyword, figuring out why def show is empty via Udemy course site

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
     validates :title, presence: true, length: {minimum: 3, maximum: 50}
     validates :description, presence: true, length: {minimum: 10, maximum: 300}
     validates :importance, numericality: {less_than_or_equal_to: 5}
+    validates :keyword, presence: true, format: { without: /\s/, message: ': One word only please' }
 end
